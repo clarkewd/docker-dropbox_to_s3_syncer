db94e687961202e1fc52c24a1ff520210e16daf5
Jeff Turner <jeff@redradishtech.com>
2015-11-30 14:46:21 +1100

    Add support for temporary 'file is uploading' marker files

--- sync_dropbox_to_s3.sh
+++ sync_dropbox_to_s3.sh
@@ -1,5 +1,12 @@
-#! /bin/bash -eu
+#! /bin/bash
 
+# Syncs local directory $SRC with aws s3 bucket $DEST. When file foo is added,
+# a temporary marker file foo.uploading.txt is created while the file is
+# uploading to S3. The marker file is deleted once the upload is completed, or
+# else a foo.uploadfailures.txt file is left with the error.
+
+#SRC=/home/jturner/coastec/dropbox_to_s3_syncer/dropbox/Dropbox
+#DEST=
 SRC=/dbox/Dropbox/"${DIRECTORY_TO_SYNC:-}"
 DEST=s3://"$S3_BUCKET"
 
@@ -11,10 +18,9 @@ function log() {
 log "$SRC to $DEST syncer started"
 
 function block_for_change {
-    log "Watching for changes in $SRC"
-    inotifywait -r \
+    inotifywait -m -r \
         -e modify,move,create,delete \
-        --exclude ".*.swp" \
+	--exclude ".*\.upload(ing|ed|failures)\.txt" \
         "$SRC"
 }
 
@@ -22,11 +28,44 @@ function sync {
         aws s3 sync "$SRC" "$DEST" --exclude .dropbox ${S3_SYNC_FLAGS:-}
 }
 
+# Creates a .uploading.txt, .uploaded.txt or .uploadfailures.txt marker file in Dropbox to indicate the status of a file upload.
+function upload_marker_file {
+	local state="$1"	# Upload status. 'begin', 'success' or an error message
+	local dir="$2"		# Upload file's dir
+	local events="$3"	# aws s3 sync event. We only care about CREATE
+	local filename="$4"	# Upload filename
+	case $events in
+	   CREATE)
+		case $state in
+		   begin)
+			if [[ $events = CREATE ]]; then
+				echo "Upload began at `date`" >> "$dir"/"$filename".uploading.txt
+			fi
+		   ;;
+		   success)
+				rm -f "$dir"/"$filename".{uploading,uploadfailures}.txt 
+				# Alternatively, if we want a permament record:
+		#		echo "Upload finished at `date`" >> "$dir"/"$filename".uploading.txt
+		#		mv "$dir"/"$filename".uploading.txt "$dir"/"$filename".uploaded.txt
+		   ;;
+		   *)
+			   # Error of some sort
+				echo -e "Upload failed at `date`:\n'$state'" >> "$dir"/"$filename".uploading.txt
+				cat "$dir"/"$filename".uploading.txt >> "$dir"/"$filename".uploadfailures.txt
+				rm "$dir"/"$filename".uploading.txt
+		esac
+	esac
+}
+
 sync
-while block_for_change
+block_for_change | while read dir events filename
 do
-    log "Sync starting"
-    sync
+    upload_marker_file begin "$dir" "$events" "$filename"
+    log "Sync starting: files altered: $filename"
+    set -vx
+    result=$(sync 2>&1)
+    [[ $? == 0 ]] && upload_marker_file success "$dir" "$events" "$filename" ||  upload_marker_file "$result" "$dir" "$events" "$filename"
+    sleep 1
     log "Sync completed"
 done
-log "$0 finished. This should not normally happen"
+log "$0 exited."
